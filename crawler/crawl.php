3f40d05c1a0b98f48f24b8de96279c86a14a59f3
Tristan Rivoallan <tristan@rivoallan.net>
2011-01-12 00:41:13 +0100

    [crawler] Implemented MongoDB storage.

--- crawler/crawl.php
+++ crawler/crawl.php
@@ -10,11 +10,28 @@ $storiesRangeStart = 1;
 $storiesRangeEnd = 1000;
 $i = $storiesRangeStart;
 
+// TODO : CLI arguments
+// TODO : drop all records
+$fullRebuild = true;
+
 // Grab stories
 while ($i <= $storiesRangeEnd)
 {
+	// Instanciate connection to Mongo
+	$mongoConnection = new Mongo();
+	$mongoDb = $mongoConnection->histoiresDeSexe;
+	$mongoCollection = $mongoDb->stories;
+
+	// Make sure story has not already been grabbed
+	if (!$fullRebuild && $mongoCollection->count(array('storyId' => $i)) > 0) {
+		logMessage(sprintf('Story %d has already been indexed, skipping.', $i));
+		$i++;
+		continue;
+	}
+
 	// Grab story
-	$crawler = $client->request('GET', sprintf('http://www.histoires-de-sexe.net/sexe.php?histoire=%d', $i));
+	$storyUrl = sprintf('http://www.histoires-de-sexe.net/sexe.php?histoire=%d', $i);
+	$crawler = $client->request('GET', $storyUrl);
 
 	// Extract interesting part of page
 	$storyNodes = $crawler->filter('.t0')->extract('_text', 'div');
@@ -32,8 +49,38 @@ while ($i <= $storiesRangeEnd)
 		$blockSentence = 'Vous êtes :';
 		$storyLines = array_slice($storyLines, 2, array_search($blockSentence, $storyLines));
 		$storyText = implode("\n", $storyLines);
+		$storyWords = array_merge(explode(' ', strtolower($storyText)), explode(' ', strtolower($storyTitle)));
 
-		logMessage(sprintf('Extracted story %d : "%s" (strlen: %d)', $i, $storyTitle, strlen($storyText)));
+		// Extract person data
+		$surnamesDatabase = array('sophie', 'marie-lou', 'julien', 'véronique', 'flor');
+		$storySurnames = array_unique(array_intersect($storyWords, $surnamesDatabase));
+
+		// Extract places data
+		$storyPlaces = array();
+
+		// Extract accessories data
+		$storyAccessories = array();
+
+		// Save in database
+		$mongoCollection->insert(array(
+			'title'       => $storyTitle, 
+			'text'        => $storyText,
+			'storyUrl'    => $storyUrl,
+			'storyId'     => $i,
+			'words'       => $storyWords,
+			'surnames'    => $storySurnames,
+			'places'      => $storyPlaces,
+			'accessories' => $storyAccessories,
+		));
+		logMessage(sprintf(
+			'Extracted story %d : "%s" (strlen: %d, surnames: %d, places: %d, accessories: %d)', 
+			$i, 
+			$storyTitle, 
+			strlen($storyText),
+			count($storySurnames),
+			count($storyPlaces),
+			count($storyAccessories)
+		));
 	} else {
 		logMessage(sprintf('No story text for story %d, skipping.', $i));
 	}
